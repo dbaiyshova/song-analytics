7b961c0dfcdab33441b954568cf3630d445fd57f
dbaiyshova <dbaiyshova@gmail.com>
2026-05-24 17:17:09 +0600

    Create streamlit project

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Song Analytics",
+    page_icon="🎵"
+)
+
+st.title("🎵 Song Analytics")
+
+st.write("My Song Analytics Portfolio Project")
+
+st.success("Application is running successfully!")
